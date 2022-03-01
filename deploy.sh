4739de411aa16ab5a3421a164a17a722a66ebb46
ciocola <acgsx@live.com>
2022-03-01 11:10:17 +0800

    change marge

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+set -e
+
+yarn build
+
+cd docs/.vuepress/dist
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git branch -M main
+
+git push -f git@github.com:ciocola/web.git main:gh-pages
+
+cd -
